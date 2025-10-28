e5cce7b85f5f158c3251d597620806a181c4ed88
Pat <pmassowalsh@gmail.com>
2025-10-28 20:16:44 +0000

    add more themes and test script

--- app/(dev)/layout.tsx
+++ app/(dev)/layout.tsx
@@ -1,6 +1,18 @@
 'use client';
 // import "./globals.css";
 import { WebflowProvidersWrapper } from "@/lib/webflow/providers";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--_font---sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--_font---mono",
+  subsets: ["latin"],
+});
+
 
 export default function RootLayout({
   children,
@@ -8,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html className="dark">
-      <body className="antialiased">
+    <html className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className="dark antialiased">
         <WebflowProvidersWrapper>
           {children}
         </WebflowProvidersWrapper>
